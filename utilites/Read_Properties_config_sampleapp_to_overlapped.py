3703b9a19df218998f8831be45610cf8b04102a4
vishalharane1 <vishalbhagwatharane@gmail.com>
2026-06-29 20:49:25 +0530

    added new testcases

---
+++ utilites/Read_Properties_config_sampleapp_to_overlapped.py
@@ -0,0 +1,19 @@
+import configparser
+
+class Read_properties_sampleapp_to_overlapp:
+    config=configparser.ConfigParser()
+    config.read(".\\Configurations\\config_sampleapp_to_overlapped.ini")
+
+    @staticmethod
+    def get_url_sampleapp():
+        return Read_properties_sampleapp_to_overlapp.config.get("URL","sampleapp")
+
+
+    @staticmethod
+    def get_url_mouseover():
+        return Read_properties_sampleapp_to_overlapp.config.get("URL", "mouseover")
+
+    @staticmethod
+    def get_url_nbsp():
+        return Read_properties_sampleapp_to_overlapp.config.get("URL", "nbsp")
+
